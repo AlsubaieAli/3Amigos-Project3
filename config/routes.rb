89a15904495bd5ef51d368def02099574c83a70c
alsubaieali <alsubaie.ali.b@gmail.com>
2018-11-29 10:52:01 +0300

    Comments, Login, and Nav are ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'comments/show'
-  get 'comments/new'
-  get 'comments/edit'
+  get "pages/home", as: "home"
   resources :stores
   devise_for :users
-  root "stores#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#home"
 end
