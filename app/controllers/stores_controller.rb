ac3aaac35195d99e34b32ba45f61ffad7d21e0e2
alanoudssr <alanoudssr@gmail.com>
2018-11-29 11:33:20 +0300

    created/completed Stores controller and Stores Views

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,14 +1,40 @@
 class StoresController < ApplicationController
   def index
-    @stores = Store.all
+    @stores = Store.all.where(category: params[:category])
   end
 
   def show
+    @store = Store.find_by(id: params[:id])
   end
 
   def new
+    @store = Store.new
+  end
+
+  def create
+    store = Store.create(store_params)
+    redirect_to stores_path
   end
 
   def edit
+    @store = Store.find_by(id: params[:id])
+  end
+
+  def update
+    store = Store.find_by(id: params[:id])
+    store.update(store_params)
+    redirect_to store_path(store)
+  end
+
+  def destroy
+    store = Store.find_by(id: params[:id])
+    store.destroy
+    redirect_to stores_path
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :address, :longitude, :latitude, :headline, :images, :price, :bio, :email, :number, :website, :facebook, :instagram, :snapchat, :twitter, :extra_link, :category, :sub_category)
   end
 end
