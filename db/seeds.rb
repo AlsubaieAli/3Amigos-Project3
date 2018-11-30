99ba061d107f9efd4aa6909750bc590140791fe2
alanoudssr <alanoudssr@gmail.com>
2018-11-30 14:32:16 +0300

    Added seeds/added styling to show page/fixed show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 Store.destroy_all
 
-Store.create(name: "Alanoud", category: "place")
-Store.create(name: "Jaffar", category: "food")
-Store.create(name: "Ali", category: "activity")
-Store.create(name: "Mansour", category: "staff")
-Store.create(name: "Mohammad", category: "place")
-Store.create(name: "Faisal", category: "decor")
-Store.create(name: "Mashael", category: "staff")
-Store.create(name: "Razan", category: "place")
-Store.create(name: "Halima", category: "decor")
+# Staff Seeds
+Store.create(name: "Bassurah", category: "staff", instagram: "https://www.instagram.com/yasserbassurah/", twitter: "https://www.twitter.com/yasserbassurah/", address: "Dammmam", snapchat: "bassurah", website: "http://www.bassurah.com/", number: "058-082-0280", headline: "‏‎مصور زواجات ومناسبات", bio: "مختص بتصوير المناسبات والزواجات. خبرة تزيد عن ٨ سنوات.", email: "Bassurah.photo@gmail.com", sub_category: "photography", images: "https://maroof.sa/images/view?q=qCuk%2fhSdaPVwPAykuCDqQvy6wf%2ft6%2fJjWkQXfYF%2fd0s%3d")
+Store.create(name: "Zawaya", category: "staff", instagram: "https://www.instagram.com/zawaya4/", twitter: "https://twitter.com/zawaya4/", address: "Riyadh", number: "059-756-9097", headline: "‏‎تغطية جميع المناسبات والأفراح", bio: "قروب شباب سعوديين مصورين نقوم بتغطية جميع المناسبات والأفراح والمناسبات العائلية مؤتمرات ، معارض ، منتجات ، مشاريع ، أشخاص متوفر لدينا تصوير مناسبات نسائية (مصورات بنات)", email: "zwaya4@outlook.com", sub_category: "photography", images: "https://maroof.sa/images/view?q=5RtKO22HdKK7dtjD1BuqodK0CuullBp0GO8UvPBhjSU%3d")
+
+#Decor Seeds
+Store.create(name: "Noni Rose", category: "decor", twitter: "https://twitter.com/NoniRoseShop/", address: "Riyadh", number: "059-559-9332", headline: "‎ نوصّل أجمل الزهور والهدايا لكم ولأحبابكم الى باب البيت", bio: "بدأنا من المنزل في تنسيق الزهور والهدايا وتوصيلها منذ ٢٠١٤ والآن أصبحنا نوصّل أجمل تنسيقات الزهور الى الرياض وجدة والشرقية ومكة والمدينة .. بالإضافة الى امريكا وبريطانيا وفرنسا", email: "Noniroseshop@gmail.com", sub_category: "flowers", images: "https://maroof.sa/images/view?q=OnoiSMrjlpmG%2fKT703Ms69DYakzmbpKe1g0%2buPWWPWk%3d")
+Store.create(name: "Tamara Ballon", category: "decor", sub_category: "balloons", address: "Riyadh", email: "nawafalmanqur@gmail.com", number: "054-000-0107", instagram: "https://www.instagram.com/tamara_ballon/", headline: "بالون واكثر ", bio: "لحفلك تميز .. ومناسباتك رونق لذالك نقدم جميع خدماتنا بجودة عالية .. جميع انواع الديكور بالبالون وتغليف الهدايا بالبالون و ديكور مجسمات الفلين .. نحن نطمح ان نكون شركاء لجمال مناسبتك ... تمارا أكثر من بالون ", images: "https://maroof.sa/images/view?q=sGR%2f62RGgNj7xOhaRoPPP9TbNpwJBU9SneeobXo8KlU%3d")
+
+#Place Seeds
+# Store.create(name: , category: "place" , sub_category: , address: , email: , number: , website: , facebook: , instagram: , snapchat:, twitter:, extra_link: , headline: , bio: , images: )
+
+#Food Seeds
+# Store.create(name: , category: "food" , sub_category: , address: , email: , number: , website: , facebook: , instagram: , snapchat:, twitter:, extra_link: , headline: , bio: , images: )
+
+#Activities Seed
+# Store.create(name: , category: "activity" , sub_category: , address: , email: , number: , website: , facebook: , instagram: , snapchat:, twitter:, extra_link: , headline: , bio: , images: )
