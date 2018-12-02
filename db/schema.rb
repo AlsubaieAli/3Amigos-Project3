d55371c75494fc87111e510f07eea82bce48dc7c
alanoudssr <alanoudssr@gmail.com>
2018-12-02 17:33:54 +0300

    added field to store model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_150531) do
+ActiveRecord::Schema.define(version: 2018_12_02_143220) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_150531) do
     t.string "sub_category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved", default: false
   end
 
   create_table "users", force: :cascade do |t|
