92e84e2bad0ba6bb5e176d2b2ceb148b26846e1a
alanoudssr <alanoudssr@gmail.com>
2018-12-04 10:41:01 +0300

    created tags views/ fixed tags controller/ created control panel for admin to modify tags in tags index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -13,4 +13,49 @@ class TagsController < ApplicationController
     @tag = Tag.find_by(id: params[:id])
     @stores = @tag.stores
   end
+
+  def new
+    @tag = Tag.new
+    unless is_admin
+      redirect_to contact_index_path
+    end
+  end
+
+  def create
+    tag = Tag.create(tag_params)
+    redirect_to tags_path
+  end
+
+  def edit
+    @tag = Tag.find_by(id: params[:id])
+    unless is_admin
+      redirect_to contact_index_path
+    end
+  end
+
+  def update
+    tag = Tag.find_by(id: params[:id])
+    tag.update(tag_params)
+    redirect_to tags_path
+  end
+
+  def destroy
+    tag = Tag.find_by(id: params[:id])
+    tag.destroy
+    redirect_to tags_path
+  end
+
+  private
+
+  def is_admin
+    if user_signed_in? && current_user.is_admin
+      return true
+    else
+      return false
+    end
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name, :category_id)
+  end
 end
