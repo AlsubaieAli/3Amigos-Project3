cac2a0917857bc2df17b419cd20c4e7fe2658966
alsubaieali <alsubaie.ali.b@gmail.com>
2018-12-04 12:47:05 +0300

    Added search functionality in the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,16 @@ class PagesController < ApplicationController
   def home
     @stores = Store.all
   end
+
+  def search
+    @stores = Store.all
+    term = params[:term]
+    results = []
+    @stores.each do |store|
+      if store.name.downcase.include?(term.downcase)
+        results.push(store)
+      end
+    end
+    render json: results
+  end
 end
